1b811c6a16a7c5001802ef82ca50b86c20643cde
Jeongkyun Jin <jeung9709@gmail.com>
2020-04-02 18:46:15 +0900

    Added preProcess py that overwrites image after preprocessing

--- source/gui.py
+++ source/gui.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import pyscreenshot as ImageGrab
+import preProcess
+import cv2
 
 master = Tk()
 
@@ -8,10 +10,15 @@ def callback():
     im = ImageGrab.grab()
 
     # save image file
-    im.save('screenshot.png')
+    filename = "Screenshot.png"
+    im.save(filename)
 
     # show image in a window
-    im.show()
+    # im.show()
+
+    # Send the image to preProcess.py for preprocessing and overwrite
+    preProcess.preProcess(filename)
+
 
 b = Button(master, text="OK", command=callback)
 b.pack()
