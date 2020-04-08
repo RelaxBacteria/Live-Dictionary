1c6fc1fbc7a157bea80cf54ad27032ce1858e434
Jeongkyun Jin <jeung9709@gmail.com>
2020-04-08 13:48:43 +0900

    Image manipulation panel with grayscaling, thresholding checkboxes and an Apply Button

--- source/gui.py
+++ source/gui.py
@@ -6,15 +6,20 @@ import ocr
 import pytesseract
 import time
 import os.path
+import tkinter.messagebox as TkMsg
 from PIL import Image
 
 # Image paths
 image_path = '../screenshot.png'
 prev_image_path = '../prev.png'
+target_image_path = '../target.png'
 
-lang_choices = ['eng', 'jpn', 'jpn_vert']
+
+
+lang_choices = ['eng', 'jpn', 'jpn_vert', 'rus', 'chi']
 bool_choices = ['yes', 'no']
-amount_choices = ['1', '2', '3']
+amount_choices = ['None', '1', '2', '3']
+
 
 class MyApp(object):
     """"""
@@ -27,12 +32,14 @@ class MyApp(object):
         self.root.geometry("420x828+611+11")
         self.frame = Tk.Frame(parent, width=300, height=300)
         self.frame.pack()
+        self.is_edited = False
 
         # GUI Design
         self.Operations_LabelFrame = Tk.LabelFrame(self.root)
         self.Operations_LabelFrame.place(relx=0.095, rely=0.013, relheight=0.175, relwidth=0.224)
         self.Operations_LabelFrame.configure(text='''Operations''')
-                 
+
+        #------- LABELFRAME ---------------------------------------------------------------------#
         captureBtn = Tk.Button(self.Operations_LabelFrame, text="Capture", command=self.capture)
         captureBtn.place(relx=0.106, rely=0.214, height=24, width=77, bordermode='ignore')
 
@@ -45,6 +52,7 @@ class MyApp(object):
         language_select = Tk.OptionMenu(self.Operations_LabelFrame, self.curr_language, *lang_choices)
         language_select.place(relx=0.106, rely=0.717, height=24, width=77, bordermode='ignore')
 
+        #------- ROOT --------------------------------------------------------------------------#
         self.text = Tk.Text(self.root)
         self.text.place(relx=0.095, rely=0.55, relheight=0.423, relwidth=0.798)
 
@@ -52,18 +60,48 @@ class MyApp(object):
         # scroll_bar.pack()
         # self.text['yscrollcommand'] = scroll_bar.set
 
-        self.prevImage = Tk.PhotoImage(file=prev_image_path)
-        self.image = Tk.Label(self.root, image=self.prevImage)
+        self.image = Tk.Label(self.root, text="Preview Image")
         self.image.place(relx=0.095, rely=0.225, height=240, width=340)
-        self.image.image = self.prevImage
 
+        #------ IMAGE MANIPULATION -----------------------------------------------------------#
         self.ImageLabelFrame = Tk.LabelFrame(self.root)
         self.ImageLabelFrame.place(relx=0.357, rely=0.013, relheight=0.17, relwidth=0.548)
         self.ImageLabelFrame.configure(text='''Image Manipulation''')
 
+        self.ApplyButton = Tk.Button(self.ImageLabelFrame, text="Apply", command=self.imageManipulate)
+        self.ApplyButton.place(relx=0.696, rely=0.78, height=24, width=57, bordermode='ignore')
+
+        # Grayscale check
+        self.grayCheck = Tk.BooleanVar()
+        self.grayCheck.set(False)
+
+        self.GrayCheck = Tk.Checkbutton(self.ImageLabelFrame, text="BW", var=self.grayCheck)
+        self.GrayCheck.place(relx=0.043, rely=0.142, relheight=0.184, relwidth=0.265, bordermode='ignore')
+
+        # Threashold drop
+        # self.Threashold_label = Tk.Label(self.ImageLabelFrame, text="Threashold: ")
+        # self.Threashold_label.place(relx=0.348, rely=0.142, relheight=0.184, relwidth=0.265, bordermode='ignore')
+
+        self.Threashold_level = Tk.BooleanVar()
+        self.Threashold_level.set(False)
+
+        self.Threashold_select = Tk.Checkbutton(self.ImageLabelFrame, text="Thresholding", var=self.Threashold_level)
+        self.Threashold_select.place(relx=0.348, rely=0.142, relheight=0.184, relwidth=0.4, bordermode='ignore')
+
+        
+
     # Method to check the option is working
     # def changeOption(self, *args):
     #     print(lang_choices[self.curr_language.get()])
+
+    def imageManipulate(self):
+        # Give the settings values on the image manipulation frame and make a target.png file, returns True if any edit was done
+        self.is_edited = preProcess.preProcess(image_path, target_image_path, self.grayCheck.get(), self.Threashold_level.get())
+        
+        if self.is_edited:
+            self.showPreviewImage(target_image_path)
+        else:
+            self.showPreviewImage()
 
     #----------------------------------------------------------------------
     def hide(self):
@@ -80,6 +118,7 @@ class MyApp(object):
 
         # save image file
         im.save(image_path)
+        self.is_edited = False
 
         # show image in a window
         # im.show()
@@ -96,13 +135,13 @@ class MyApp(object):
         finally:
             self.show()
     
-    def showPreviewImage(self):
+    def showPreviewImage(self, image=image_path):
         # Make a preview image
-        preProcess.createPreview(image_path, prev_image_path)
+        preProcess.createPreview(image, prev_image_path)
         
         # Load and show the preview Image
         self.prevImage = Tk.PhotoImage(file=prev_image_path)
-        self.image.configure(image=self.prevImage)
+        self.image.configure(image=self.prevImage, text="")
         self.image.image = self.prevImage
 
     def ocr(self):
@@ -110,9 +149,18 @@ class MyApp(object):
         self.text.delete(0.0, Tk.END)
 
         # Display results in the textbox
-        self.text.insert(Tk.CURRENT, pytesseract.image_to_string(Image.open(image_path), lang=self.curr_language.get()))
+        try:
+            if self.is_edited:
+                self.text.insert(Tk.CURRENT, pytesseract.image_to_string(Image.open(target_image_path), lang=self.curr_language.get()))
+            else:
+                self.text.insert(Tk.CURRENT, pytesseract.image_to_string(Image.open(image_path), lang=self.curr_language.get()))
+
+        except pytesseract.pytesseract.TesseractError as t:
+            self.alert('No train data for the language was detacted.')
         
-        
+    def alert(self, message):
+        TkMsg.showwarning("Warning!", message)
+
     #----------------------------------------------------------------------
     def show(self):
         """"""
--- source/preProcess.py
+++ source/preProcess.py
@@ -1,69 +1,40 @@
+"""
+
+"""
+
 import cv2
 import numpy as np
 
-def createPreview(image_name, target_name):
-    # Load screenshot.png
-    image = cv2.imread(image_name)
 
-    # Shrink it and create preview.png
-    preview_image = cv2.resize(image, dsize=(480, 320), fx=0.3, fy=0.7, interpolation=cv2.INTER_LINEAR)
+def createPreview(image_name, prev_image_name):
+    src = cv2.imread(image_name, cv2.IMREAD_COLOR)
+
+    prev = cv2.resize(src, dsize=(340, 240), fx=0.3, fy=0.7, interpolation=cv2.INTER_LINEAR)
+
+    cv2.imwrite(prev_image_name, prev)
+
+def preProcess(image_name, target_image_name, gray, threashold):
+    # Check Parameters
+    # print("gray: {gray}, threashold: {threashold}".format(gray=gray, threashold=threashold))
+
+    # Import image to edit
+    src = cv2.imread(image_name, cv2.IMREAD_COLOR)
+
+    is_edited = False
+
+    if gray:
+        src = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+        is_edited = True
     
-    cv2.imwrite(target_name, preview_image)
+    if (gray & threashold):
+        # src = cv2.adaptiveThreshold(src, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 2)
+        ret, src = cv2.threshold(src, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        is_edited = True
 
-def preProcess(image_name):
-    image = cv2.imread(image_name)
+    # Make target.png
+    cv2.imwrite(target_image_name, src)
 
-    def get_grayscale(image):
-        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    print('Returning: ', is_edited) 
+    return is_edited
 
-    # noise removal
-    def remove_noise(image):
-        return cv2.medianBlur(image,5)
     
-    #thresholding
-    def thresholding(image):
-        return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
-    #dilation
-    def dilate(image):
-        kernel = np.ones((5,5),np.uint8)
-        return cv2.dilate(image, kernel, iterations = 1)
-        
-    #erosion
-    def erode(image):
-        kernel = np.ones((5,5),np.uint8)
-        return cv2.erode(image, kernel, iterations = 1)
-
-    #opening - erosion followed by dilation
-    def opening(image):
-        kernel = np.ones((5,5),np.uint8)
-        return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-
-    #canny edge detection
-    def canny(image):
-        return cv2.Canny(image, 100, 200)
-
-    #skew correction
-    def deskew(image):
-        coords = np.column_stack(np.where(image > 0))
-        angle = cv2.minAreaRect(coords)[-1]
-        if angle < -45:
-            angle = -(90 + angle)
-        else:
-            angle = -angle
-        (h, w) = image.shape[:2]
-        center = (w // 2, h // 2)
-        M = cv2.getRotationMatrix2D(center, angle, 1.0)
-        rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-        return rotated
-
-    #template matching
-    def match_template(image, template):
-        return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
-    
-    # Process
-    processed1 = get_grayscale(image)
-    processed2 = thresholding(processed1)
-
-    # Overwrite the image
-    cv2.imwrite(image_name, processed2)
